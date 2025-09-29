bb97c4222c2b85676a0ab210f9b6a42ba992448d
uchida-atsu <uchida.atsuro@gmail.com>
2025-09-29 15:04:31 +0900

    検索機能の追加とそのテスト実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -13,7 +13,14 @@ class TweetController extends Controller
     public function index()
     {
         //
-        $tweets = Tweet::with(['user', 'liked'])->latest()->get();
+        // $query = Tweet::query();
+        // // ページネーションを追加（1ページに10件表示）
+        // $tweets = $query
+        //     ->latest()
+        //     ->paginate(10);
+
+
+        $tweets = Tweet::with(['user', 'liked'])->latest()->paginate(10);
         return view('tweets.index', compact('tweets'));
     }
 
@@ -79,5 +86,30 @@ class TweetController extends Controller
         $tweet->delete();
 
         return redirect()->route('tweets.index');
+    }
+
+    /**
+     * Search for tweets containing the keyword.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View
+     */
+    public function search(Request $request)
+    {
+
+        $query = Tweet::query();
+
+        // キーワードが指定されている場合のみ検索を実行
+        if ($request->filled('keyword')) {
+            $keyword = $request->keyword;
+            $query->where('tweet', 'like', '%' . $keyword . '%');
+        }
+
+        // ページネーションを追加（1ページに10件表示）
+        $tweets = $query
+            ->latest()
+            ->paginate(10);
+
+        return view('tweets.search', compact('tweets'));
     }
 }
